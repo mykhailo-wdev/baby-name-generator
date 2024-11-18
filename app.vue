744b91ae82e4fd7f10985092b06489c8d2eaf8e3
Mike Mike <mike_insider@proton.me>
2024-11-18 19:30:14 +0200

    Creating the name cards

--- app.vue
+++ app.vue
@@ -67,8 +67,13 @@
         </div>
       </div>
       <button class="primary" @click="cumputeSelectedNames">Find Names</button>
+      <div class="cards-container">
+        <div v-for="name in selectedNames" :key="name" class="card">
+          <h4>{{ name }}</h4>
+          <p>x</p>
+        </div>
+      </div>
     </div>
-    {{ selectedNames }}
   </div>
 </template>
 
@@ -170,5 +175,30 @@ h1 {
   font-size: 1rem;
   margin-top: 1rem;
   border-radius: 6.5rem;
+}
+
+.cards-container {
+  display: flex;
+  margin-top: 3rem;
+  flex-wrap: wrap;
+}
+
+.card {
+  background-color: rgb(27, 60 , 138);
+  width: 28%;
+  color: rgb(255, 255, 255);
+  border-radius: 0.3rem;
+  padding: 0.3rem;
+  margin-right: 0.5rem;
+  margin-bottom: 1rem;
+  position: relative;
+}
+
+.card p {
+  position: absolute;
+  top: -20%;
+  left: 92.5%;
+  cursor: pointer;
+  color: rgba(255, 255, 255, 0.178);
 }
 </style>
